d8b8f15f269a40c104fd2c64df7e22d17a577005
Luiz Fernando Meira Filho <fernandomeira@Luizs-MacBook-Pro.local>
2021-01-10 15:20:06 -0700

    spun off the updateLastEventIndex flow to a standalone script to be reused, added to querySystem main script

---
+++ src/querySystem/handlers/updateLastEventTimestamps.tsx
@@ -0,0 +1,21 @@
+import low from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+import chalk from "chalk";
+import path from "path";
+import { Schema } from "../dbTypes";
+import _ from "lodash";
+import { getLastTimestamps } from "../graphql/queries";
+
+const db_path = path.resolve(__dirname, "../../../../src/db/db.json");
+
+const updateLastEventsTimestamps: any = async () => {
+  const adapter = new FileSync<Schema>(db_path);
+  const db = await low(adapter);
+
+  const latestTimestamps = await getLastTimestamps();
+  db.set("lastEventTimestamps", latestTimestamps).write();
+
+  console.log(chalk.magenta("Updated last event timestamps!"));
+};
+
+export default updateLastEventsTimestamps;
--- src/querySystem/querySystem.tsx
+++ src/querySystem/querySystem.tsx
@@ -5,51 +5,46 @@ import chalk from "chalk";
 import path from "path";
 import { promises as fsPromises } from "fs";
 import { Schema } from "./dbTypes";
-import { getLastTimestamps, loadInitialUsers } from "./graphql/queries";
+import { loadInitialUsers } from "./graphql/queries";
+import updateLastEventsTimestamps from "./handlers/updateLastEventTimestamps";
 
 // types for the db schema
-
-// import { loadInitialUsers } from "./graphql/queries";
 
 // GOTCHA: path relative to the build folder..
 const db_path = path.resolve(__dirname, "../../../src/db/db.json");
 
 async function start() {
   try {
-    console.log(chalk.greenBright("### Starting Query System .. ###"));
+    console.log(chalk.bold.greenBright("### Starting Query System .. ###"));
 
     //check if db.json exists, if not, create db and initial run config
     if (!fs.existsSync(db_path)) {
       await fsPromises.writeFile(db_path, "");
       console.log(chalk.yellow("Created new DB file "));
+
+      //create default format for db
+      const adapter = new FileSync<Schema>(db_path);
+      const db = await low(adapter);
+
+      db.defaults({
+        users: [],
+        lastEventTimestamps: [],
+        // generalReserves: [],
+      }).write();
+
+      // insert into db timestamp of the last update
+      console.log(chalk.green("Loading initial data.."));
+
+      updateLastEventsTimestamps();
+
+      // fill with with all users
+
+      const initialUsers: any = await loadInitialUsers();
+
+      db.set("users", [initialUsers[0]]).write();
     }
 
-    //create default format for db
-    const adapter = new FileSync<Schema>(db_path);
-    const db = await low(adapter);
-
-    db.defaults({
-      users: [],
-      lastEventTimestamp: [],
-      // generalReserves: [],
-    }).write();
-
-    // insert into db timestamp of the last update
-    console.log(chalk.green("Loading initial data.."));
-
-    const latestTimestamps = await getLastTimestamps();
-    db.set("lastEventTimestamp", latestTimestamps).write();
-
-    // fill with with all users
-
-    const initialUsers: any = await loadInitialUsers();
-
-    db.get("users")
-      .push(initialUsers[0])
-      .push(initialUsers[1])
-      .write();
-
-    // db.set("users[0]").write();
+    console.log(chalk.bold.greenBright("### Starting Watch Mode .. ###"));
 
     // start querying for events of interest using the last index from
     // previous step
