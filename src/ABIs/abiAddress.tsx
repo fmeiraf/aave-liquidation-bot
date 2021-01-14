4a1851f36d6d2a8146fb8f5cab410e65800a1573
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-01-13 22:08:04 -0700

    added abi files plus contract addresses placholder to easily export contracts using objects

---
+++ src/ABIs/abiAddress.tsx
@@ -0,0 +1,12 @@
+const abiAddress = {
+  ProtocolDataProvider: {
+    kovan: "0x3c73A5E5785cAC854D468F727c606C07488a29D6",
+    mainnet: "",
+  },
+  LendingPool: {
+    kovan: "0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe",
+    mainnet: "",
+  },
+};
+
+export default abiAddress;
---
+++ src/calcSystem/tests/effectTesting.tsx
@@ -0,0 +1,8 @@
+import { ethers } from "ethers";
+import abiAddress from "../../ABIs/abiAddress";
+import LendingPool from "../../ABIs/LendingPool.json";
+
+const myTestAddres = "0xAeA2dF19506eA7bc1b3AA82f29a3115c77f0c21e";
+const myTestAddresLower = "0xaea2df19506ea7bc1b3aa82f29a3115c77f0c21e";
+
+const lendingPoolABI = "../../ABIs/LendingPool";
