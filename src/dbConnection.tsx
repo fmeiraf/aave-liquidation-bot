6cb126ec546bba602fed829e2cb44ae68593f6f7
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-01-14 23:00:27 -0700

    added file to export db connection

---
+++ src/dbConnection.tsx
@@ -0,0 +1,11 @@
+import low from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+import path from "path";
+import { Schema } from "../querySystem/dbTypes";
+
+const db_path = path.resolve(__dirname, "../../../src/db/db.json");
+
+const adapter = new FileSync<Schema>(db_path);
+const dbConn = low(adapter);
+
+export default dbConn;
