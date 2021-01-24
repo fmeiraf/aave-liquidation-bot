19bcf198b4c83e8711b3c783534b4e4e9c67412b
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-01-23 18:11:31 -0700

    chaged tests to check decimal numbers accuracy

--- src/calcSystem/tests/kovan/userReserveTest.tsx
+++ src/calcSystem/tests/kovan/userReserveTest.tsx
@@ -5,6 +5,7 @@ import {
 } from "./kovanTestConfig";
 import { poolReserve } from "../../../querySystem/dbTypes";
 import { computeUserReserveData } from "../../helpers/main-calcs";
+import { normalizeNoDecimals } from "../../helpers/pool-math";
 
 import { ethers } from "ethers";
 import dbConn from "../../../dbConnection";
@@ -48,52 +49,102 @@ const userReserveTest = async () => {
   let variableDebtResult;
 
   // aToken testing
+  const currATokenChain = userReserveOnChain["currentATokenBalance"].toString();
+  let eqAChain = 0;
   if (userDaiReserve["scaledATokenBalance"] === "0") {
     atokenResult =
       userReserveOnChain["currentATokenBalance"].toString() === "0";
   } else {
-    atokenResult =
-      userReserveOnChain["currentATokenBalance"].toString().substr(0, 6) ===
-      computedUserReserve["scaledATokenBalance"].substr(0, 6);
+    for (let i = 0; i < currATokenChain.length; i++) {
+      if (computedUserReserve["underlyingBalance"][i] === currATokenChain[i]) {
+        eqAChain++;
+      } else {
+        break;
+      }
+    }
   }
+
+  const aNonDecimalSize = normalizeNoDecimals(
+    userReserveOnChain["currentATokenBalance"].toString(),
+    18
+  ).length;
+
+  if (atokenResult === undefined) {
+    atokenResult = eqAChain >= aNonDecimalSize; //covers at least integer pasrt
+  }
+
   console.log(
-    `Checking aToken calculation: ${
+    `Checking A Token calculation: ${
       atokenResult ? chalk.bold.green("PASS!") : chalk.bold.red("FAIL!")
-    } `
+    }, with ${eqAChain - aNonDecimalSize} decimals of accuracy.`
   );
 
   // stableDebt testing
+  const currStableDebtChain = userReserveOnChain[
+    "currentStableDebt"
+  ].toString();
+  let eqCountStable = 0;
   if (userDaiReserve["principalStableDebt"] === "0") {
-    stableDebtResult =
-      userReserveOnChain["currentStableDebt"].toString() === "0";
+    stableDebtResult = currStableDebtChain === "0";
   } else {
-    stableDebtResult =
-      userReserveOnChain["currentStableDebt"].toString().substr(0, 6) ===
-      computedUserReserve["stableBorrows"].substr(0, 6);
+    for (let i = 0; i < currStableDebtChain.length; i++) {
+      if (computedUserReserve["stableBorrows"][i] === currStableDebtChain[i]) {
+        eqCountStable++;
+      } else {
+        break;
+      }
+    }
   }
+
+  const stableNonDecimalSize = normalizeNoDecimals(
+    userReserveOnChain["currentStableDebt"].toString(),
+    18
+  ).length;
+
+  if (stableDebtResult === undefined) {
+    stableDebtResult = eqCountStable >= stableNonDecimalSize; //covers at least integer pasrt
+  }
+
   console.log(
     `Checking Stable Debt calculation: ${
       stableDebtResult ? chalk.bold.green("PASS!") : chalk.bold.red("FAIL!")
-    } `
+    }, with ${eqCountStable - stableNonDecimalSize} decimals of accuracy.`
   );
 
   // variable debt testing
+  const currVariableDebtChain = userReserveOnChain[
+    "currentVariableDebt"
+  ].toString();
+  let eqCountVar = 0;
   if (userDaiReserve["scaledVariableDebt"] === "0") {
     variableDebtResult =
       userReserveOnChain["currentVariableDebt"].toString() === "0";
   } else {
-    variableDebtResult =
-      userReserveOnChain["currentVariableDebt"].toString().substr(0, 6) ===
-      computedUserReserve["variableBorrows"].substr(0, 6);
+    for (let i = 0; i < currVariableDebtChain.length; i++) {
+      if (
+        computedUserReserve["variableBorrows"][i] === currVariableDebtChain[i]
+      ) {
+        eqCountVar++;
+      } else {
+        break;
+      }
+    }
   }
+
+  const variableNonDecimalSize = normalizeNoDecimals(
+    userReserveOnChain["currentVariableDebt"].toString(),
+    18
+  ).length;
+
+  if (variableDebtResult === undefined) {
+    variableDebtResult = eqCountVar >= variableNonDecimalSize; //covers at least integer part
+  }
+
   console.log(
     `Checking Variable Debt calculation: ${
       variableDebtResult ? chalk.bold.green("PASS!") : chalk.bold.red("FAIL!")
-    } `
+    }, with ${eqCountVar - variableNonDecimalSize} decimals of accuracy.`
   );
-
-  console.log(userReserveOnChain["currentVariableDebt"].toString());
-  console.log(computedUserReserve["variableBorrows"]);
 
   return [computedUserReserve, userReserveOnChain];
 };
