dadad519c0147c5e05320e191f863ec494375ab5
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-01-30 17:35:06 -0700

    changed hardhar dirs to facilitate running tests

--- src/eth/hardhat.config.ts
+++ src/eth/hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-waffle";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
+  console.log(args);
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
