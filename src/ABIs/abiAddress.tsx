6a7b62f8fe6a7b856af58cf724f3cb8724f928c6
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-02-01 22:41:15 -0700

    added priceOracle contract ABI and address

--- src/ABIs/abiAddress.tsx
+++ src/ABIs/abiAddress.tsx
@@ -7,6 +7,10 @@ const abiAddress = {
     kovan: "0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe",
     mainnet: "",
   },
+  PriceOracle: {
+    kovan: "0xB8bE51E6563BB312Cbb2aa26e352516c25c26ac1",
+    mainnet: "0xA50ba011c48153De246E5192C8f9258A2ba79Ca9",
+  },
 };
 
 export default abiAddress;
--- src/env.tsx
+++ src/env.tsx
@@ -2,5 +2,5 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const NETWORK: any = process.env.NETWORK;
-export const INFURA_KOVAN: any = process.env.INFURA_KOVAN;
+export const NETWORK: any | string = process.env["NETWORK"];
+export const INFURA_KOVAN: any | string = process.env["INFURA_KOVAN"];
