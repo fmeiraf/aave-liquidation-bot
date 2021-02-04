6701f8222da89a856cb1802f2d6c0e1d86c0dfe1
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-02-03 22:10:09 -0700

    Added addresses for mainnet contracts

--- src/ABIs/abiAddress.tsx
+++ src/ABIs/abiAddress.tsx
@@ -1,11 +1,11 @@
 const abiAddress = {
   ProtocolDataProvider: {
     kovan: "0x3c73A5E5785cAC854D468F727c606C07488a29D6",
-    mainnet: "",
+    mainnet: "0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d",
   },
   LendingPool: {
     kovan: "0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe",
-    mainnet: "",
+    mainnet: "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9",
   },
   PriceOracle: {
     kovan: "0xB8bE51E6563BB312Cbb2aa26e352516c25c26ac1",
