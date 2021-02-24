bcbb7ab7093b776c5d8f78b6c8ef43b5a872a977
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2021-02-23 22:00:39 -0700

    added RouterV2 uniswap ABI

--- src/ABIs/abiAddress.tsx
+++ src/ABIs/abiAddress.tsx
@@ -15,6 +15,10 @@ const abiAddress = {
     kovan: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
     mainnet: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
   },
+  UniswapV2Router02: {
+    kovan: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    mainnet: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+  },
 };
 
 export default abiAddress;
